aa1a08518a6b02e30e0a257e731ea8711c8250f7
lack <valentin.dautrement@protonmail.com>
2025-06-12 10:20:14 +0200

    feat(user): user crud and token returned in response

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
+use App\DTO\UserUpdateDTO;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class UserController extends AbstractController
 {
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
     #[Route('/api/user', name: 'app_user', methods: ['GET'])]
     public function get_user(): Response
     {
+        /** @var User $user */
         $user = $this->getUser();
 
         if (!$user) {
@@ -27,4 +44,99 @@ final class UserController extends AbstractController
             'description' => $user->getDescription(),
         ]);
     }
+
+    #[Route('/api/user', name: 'app_user_update', methods: ['PUT'])]
+    public function update_user(
+        Request $request,
+        JWTTokenManagerInterface $JWTManager,
+        UserRepository $userRepository,
+        TranslatorInterface $translator,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator
+    ): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->json(['error' => $translator->trans('user.not_found')], Response::HTTP_UNAUTHORIZED);
+        }
+
+        try {
+            /** @var UserUpdateDTO $userDTO */
+            $userDTO = $serializer->deserialize(
+                $request->getContent(),
+                UserUpdateDTO::class,
+                'json'
+            );
+        } catch (\Exception $e) {
+            return $this->json(['error' => $translator->trans('invalid_json_format')], Response::HTTP_BAD_REQUEST);
+        }
+
+        $errors = $validator->validate($userDTO);
+        if (count($errors) > 0) {
+            $errorMessages = [];
+            foreach ($errors as $error) {
+                $errorMessages[] = $error->getMessage();
+            }
+            return $this->json(['errors' => $errorMessages], Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($userDTO->getEmail() && $userDTO->getEmail() !== $user->getEmail()) {
+            if ($userRepository->findOneBy(['email' => $userDTO->getEmail()])) {
+                return $this->json(['error' => $translator->trans('user.already_exists')], Response::HTTP_BAD_REQUEST);
+            }
+            $user->setEmail($userDTO->getEmail());
+        }
+
+        if ($userDTO->getUsername() && $userDTO->getUsername() !== $user->getUsername()) {
+            if ($userRepository->findOneBy(['username' => $userDTO->getUsername()])) {
+                return $this->json(['error' => $translator->trans('user.username_taken')], Response::HTTP_BAD_REQUEST);
+            }
+            $user->setUsername($userDTO->getUsername());
+        }
+
+        if ($userDTO->getPassword()) {
+            $user->setPassword($this->passwordHasher->hashPassword($user, $userDTO->getPassword()));
+        }
+
+        if ($userDTO->getFirstName()) {
+            $user->setFirstName($userDTO->getFirstName());
+        }
+
+        if ($userDTO->getLastName()) {
+            $user->setLastName($userDTO->getLastName());
+        }
+
+        if ($userDTO->getDescription()) {
+            $user->setDescription($userDTO->getDescription());
+        }
+
+        if ($userDTO->getProfilePicture()) {
+            $user->setProfilePicture($userDTO->getProfilePicture());
+        }
+
+        $this->entityManager->flush();
+
+        return $this->json([
+            'message' => $translator->trans('user.updated_successfully')
+        ], Response::HTTP_OK);
+    }
+
+    #[Route('/api/user', name: 'app_user_delete', methods: ['DELETE'])]
+    public function delete_user(
+        TranslatorInterface $translator
+    ): Response {
+        /** @var User $user */
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->json(['error' => $translator->trans('user.not_found')], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+
+        return $this->json(['message' => $translator->trans('user.deleted_successfully')], Response::HTTP_OK);
+    }
 }
+
+
