991abf42b29519783c3853541611d03abe81a8ba
lack <valentin.dautrement@protonmail.com>
2025-06-24 12:13:17 +0200

    hotfix(fixtures): image on fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -51,7 +51,7 @@ class AppFixtures extends Fixture
         $card1 = new Card();
         $card1->setName('Forest Guardian');
         $card1->setDescription('A mystical protector of ancient woodlands.');
-        $card1->setImage('forest_guardian.png');
+        $card1->setImage('images/CARD_PLACEHOLDER.png');
         $card1->setArtistTag('artist1');
         $card1->setRarity(CardRarity::COMMON);
         $card1->setReleaseDate(new \DateTimeImmutable());
@@ -62,7 +62,7 @@ class AppFixtures extends Fixture
         $card2 = new Card();
         $card2->setName('Thunder Raptor');
         $card2->setDescription('A lightning-fast bird that creates sonic booms.');
-        $card2->setImage('thunder_raptor.png');
+        $card2->setImage('images/CARD_PLACEHOLDER.png');
         $card2->setArtistTag('artist2');
         $card2->setRarity(CardRarity::UNCOMMON);
         $card2->setReleaseDate(new \DateTimeImmutable());
@@ -73,7 +73,7 @@ class AppFixtures extends Fixture
         $card3 = new Card();
         $card3->setName('Abyssal Horror');
         $card3->setDescription('Eldritch terror from the deepest voids.');
-        $card3->setImage('abyssal_horror.png');
+        $card3->setImage('images/CARD_PLACEHOLDER.png');
         $card3->setArtistTag('artist3');
         $card3->setRarity(CardRarity::RARE);
         $card3->setReleaseDate(new \DateTimeImmutable());
@@ -84,7 +84,7 @@ class AppFixtures extends Fixture
         $card4 = new Card();
         $card4->setName('Crystal Golem');
         $card4->setDescription('Living mineral formation that reflects magic.');
-        $card4->setImage('crystal_golem.png');
+        $card4->setImage('images/CARD_PLACEHOLDER.png');
         $card4->setArtistTag('artist1');
         $card4->setRarity(CardRarity::COMMON);
         $card4->setReleaseDate(new \DateTimeImmutable());
@@ -95,7 +95,7 @@ class AppFixtures extends Fixture
         $card5 = new Card();
         $card5->setName('Celestial Seraph');
         $card5->setDescription('Divine being of pure cosmic energy.');
-        $card5->setImage('celestial_seraph.png');
+        $card5->setImage('images/CARD_PLACEHOLDER.png');
         $card5->setArtistTag('artist2');
         $card5->setRarity(CardRarity::EPIC);
         $card5->setReleaseDate(new \DateTimeImmutable());
@@ -106,7 +106,7 @@ class AppFixtures extends Fixture
         $card6 = new Card();
         $card6->setName('Inferno Titan');
         $card6->setDescription('Mountain-sized embodiment of volcanic fury.');
-        $card6->setImage('inferno_titan.png');
+        $card6->setImage('images/CARD_PLACEHOLDER.png');
         $card6->setArtistTag('artist3');
         $card6->setRarity(CardRarity::MYTHIC);
         $card6->setReleaseDate(new \DateTimeImmutable());
@@ -117,7 +117,7 @@ class AppFixtures extends Fixture
         $card7 = new Card();
         $card7->setName('Venomous Shade');
         $card7->setDescription('Spectral assassin that poisons with touch.');
-        $card7->setImage('venomous_shade.png');
+        $card7->setImage('images/CARD_PLACEHOLDER.png');
         $card7->setArtistTag('artist1');
         $card7->setRarity(CardRarity::UNCOMMON);
         $card7->setReleaseDate(new \DateTimeImmutable());
@@ -128,7 +128,7 @@ class AppFixtures extends Fixture
         $card8 = new Card();
         $card8->setName('Tundra Wolf');
         $card8->setDescription('Alpha predator of frozen wastelands.');
-        $card8->setImage('tundra_wolf.png');
+        $card8->setImage('images/CARD_PLACEHOLDER.png');
         $card8->setArtistTag('artist2');
         $card8->setRarity(CardRarity::COMMON);
         $card8->setReleaseDate(new \DateTimeImmutable());
@@ -139,7 +139,7 @@ class AppFixtures extends Fixture
         $card9 = new Card();
         $card9->setName('Chrono Mage');
         $card9->setDescription('Manipulator of time and temporal forces.');
-        $card9->setImage('chrono_mage.png');
+        $card9->setImage('images/CARD_PLACEHOLDER.png');
         $card9->setArtistTag('artist3');
         $card9->setRarity(CardRarity::RARE);
         $card9->setReleaseDate(new \DateTimeImmutable());
@@ -150,7 +150,7 @@ class AppFixtures extends Fixture
         $card10 = new Card();
         $card10->setName('Spectral Pirate');
         $card10->setDescription('Ghostly raider of the ethereal seas.');
-        $card10->setImage('spectral_pirate.png');
+        $card10->setImage('images/CARD_PLACEHOLDER.png');
         $card10->setArtistTag('artist1');
         $card10->setRarity(CardRarity::UNCOMMON);
         $card10->setReleaseDate(new \DateTimeImmutable());
@@ -161,7 +161,7 @@ class AppFixtures extends Fixture
         $card11 = new Card();
         $card11->setName('Ironwood Treant');
         $card11->setDescription('Ancient tree-being with bark like steel.');
-        $card11->setImage('ironwood_treant.png');
+        $card11->setImage('images/CARD_PLACEHOLDER.png');
         $card11->setArtistTag('artist2');
         $card11->setRarity(CardRarity::COMMON);
         $card11->setReleaseDate(new \DateTimeImmutable());
@@ -172,7 +172,7 @@ class AppFixtures extends Fixture
         $card12 = new Card();
         $card12->setName('Anniversary Emblem');
         $card12->setDescription('Commemorative card for the game\'s anniversary.');
-        $card12->setImage('anniversary_emblem.png');
+        $card12->setImage('images/CARD_PLACEHOLDER.png');
         $card12->setArtistTag('artist3');
         $card12->setRarity(CardRarity::SPECIAL);
         $card12->setReleaseDate(new \DateTimeImmutable());
@@ -183,7 +183,7 @@ class AppFixtures extends Fixture
         $card13 = new Card();
         $card13->setName('Convention Exclusive');
         $card13->setDescription('Rare promotional card from gaming conventions.');
-        $card13->setImage('convention_exclusive.png');
+        $card13->setImage('images/CARD_PLACEHOLDER.png');
         $card13->setArtistTag('artist1');
         $card13->setRarity(CardRarity::PROMO);
         $card13->setReleaseDate(new \DateTimeImmutable());
@@ -194,7 +194,7 @@ class AppFixtures extends Fixture
         $card14 = new Card();
         $card14->setName('First Edition Mark');
         $card14->setDescription('Limited first printing of a foundational card.');
-        $card14->setImage('first_edition_mark.png');
+        $card14->setImage('images/CARD_PLACEHOLDER.png');
         $card14->setArtistTag('artist2');
         $card14->setRarity(CardRarity::LIMITED);
         $card14->setReleaseDate(new \DateTimeImmutable());
@@ -205,7 +205,7 @@ class AppFixtures extends Fixture
         $card15 = new Card();
         $card15->setName('Desert Nomad');
         $card15->setDescription('Survivor who thrives in arid wastelands.');
-        $card15->setImage('desert_nomad.png');
+        $card15->setImage('images/CARD_PLACEHOLDER.png');
         $card15->setArtistTag('artist3');
         $card15->setRarity(CardRarity::COMMON);
         $card15->setReleaseDate(new \DateTimeImmutable());
@@ -216,7 +216,7 @@ class AppFixtures extends Fixture
         $card16 = new Card();
         $card16->setName('Plasma Elemental');
         $card16->setDescription('Living embodiment of raw electrical energy.');
-        $card16->setImage('plasma_elemental.png');
+        $card16->setImage('images/CARD_PLACEHOLDER.png');
         $card16->setArtistTag('artist1');
         $card16->setRarity(CardRarity::UNCOMMON);
         $card16->setReleaseDate(new \DateTimeImmutable());
@@ -227,7 +227,7 @@ class AppFixtures extends Fixture
         $card17 = new Card();
         $card17->setName('Mercenary Captain');
         $card17->setDescription('Battle-hardened leader of sellswords.');
-        $card17->setImage('mercenary_captain.png');
+        $card17->setImage('images/CARD_PLACEHOLDER.png');
         $card17->setArtistTag('artist2');
         $card17->setRarity(CardRarity::COMMON);
         $card17->setReleaseDate(new \DateTimeImmutable());
@@ -238,7 +238,7 @@ class AppFixtures extends Fixture
         $card18 = new Card();
         $card18->setName('Necropolis Lord');
         $card18->setDescription('Undead ruler of a city of bones.');
-        $card18->setImage('necropolis_lord.png');
+        $card18->setImage('images/CARD_PLACEHOLDER.png');
         $card18->setArtistTag('artist3');
         $card18->setRarity(CardRarity::RARE);
         $card18->setReleaseDate(new \DateTimeImmutable());
@@ -249,7 +249,7 @@ class AppFixtures extends Fixture
         $card19 = new Card();
         $card19->setName('Sky Fortress');
         $card19->setDescription('Floating bastion of aerial domination.');
-        $card19->setImage('sky_fortress.png');
+        $card19->setImage('images/CARD_PLACEHOLDER.png');
         $card19->setArtistTag('artist1');
         $card19->setRarity(CardRarity::EPIC);
         $card19->setReleaseDate(new \DateTimeImmutable());
@@ -260,7 +260,7 @@ class AppFixtures extends Fixture
         $card20 = new Card();
         $card20->setName('Goblin Alchemist');
         $card20->setDescription('Unstable experimenter with dangerous potions.');
-        $card20->setImage('goblin_alchemist.png');
+        $card20->setImage('images/CARD_PLACEHOLDER.png');
         $card20->setArtistTag('artist2');
         $card20->setRarity(CardRarity::COMMON);
         $card20->setReleaseDate(new \DateTimeImmutable());
