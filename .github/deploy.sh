f64f2ae6658c09645d0308264c532fb623bb9fed
Maximauve <max.mourgues@gmail.com>
2025-07-04 09:20:37 +0200

    TCG-98 : CI & deploy back

---
+++ .github/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+composer install --no-dev --optimize-autoloader
+if [ $? -ne 0 ]; then
+    echo "Composer install failed"
+    exit 1
+fi
+php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
+if [ $? -ne 0 ]; then
+    echo "Migration failed"
+    exit 1
+fi
+php bin/console importmap:install
+if [ $? -ne 0 ]; then
+    echo "Import map install failed"
+    exit 1
+fi
+php bin/console cache:clear
+php bin/console cache:pool:clear --all
